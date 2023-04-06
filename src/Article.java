2649382d9760c06f1b343075ec5e79772db5f0e4
alhatmii <alhatmii.150@gmail.com>
2023-04-06 11:34:29 +0400

    Adding Article Class

---
+++ src/Article.java
@@ -0,0 +1,49 @@
+public class Article {
+
+	private String title;
+	private String author;
+	private String date;
+	private String category;
+	private String content;
+
+	// Constructors, getters, and setters
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
